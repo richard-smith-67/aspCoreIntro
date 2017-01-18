9685fcb2da466d1268f0f7ca83e5e920adcf36ce
Richard Smith <richard.smith.67@gmail.com>
2017-01-18 01:28:35 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class StartUp {
+        
+        public void Configure(IApplicationBuilder app) {
+            
+            app.Run(context => {
+                return context.Response.WriteAsync("hi roll");    
+            });
+
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<StartUp>()
+            .Build();
+
+            host.Run();
+        }
+    }
+}
